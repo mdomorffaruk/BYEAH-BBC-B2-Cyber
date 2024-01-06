c0bb0b089f4d842333e2d451eca1a505c1e9c24a
MOHAMMAD OMOR FARUK <63899148+mdomorffaruk@users.noreply.github.com>
2024-01-06 12:19:02 +0600

    Update email_cli.py

--- IM-W7-M1/email_cli.py
+++ IM-W7-M1/email_cli.py
@@ -13,7 +13,7 @@ def sentEmail():
     
     smtp_server= "smtp.gmail.com"
     smtp_port=465
-    sender_auth_key="mnhdnepqigtjxqwe"
+    sender_auth_key="YOUR)EMAIL_AUTH_KEY"
 
     # Data from input: 
     recipients_email = input("Enter email: ")
